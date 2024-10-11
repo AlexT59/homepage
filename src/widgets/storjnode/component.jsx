1728622178dee9a043d07022b70ec92727371fa2
Alexandre TRUPIN <alex.trupin@gmail.com>
2024-10-11 17:12:53 +0200

    add online score

--- src/widgets/storjnode/component.jsx
+++ src/widgets/storjnode/component.jsx
@@ -11,7 +11,7 @@ export default function Component({ service }) {
 
   const { data: dashData, error: dashError } = useWidgetAPI(widget, "dashboard");
   const { data: payoutData, error: payoutError } = useWidgetAPI(widget, "payout");
-  const { data: satelData, error satelError } = useWidgetAPI(widget, "satellites");
+  const { data: satelData, error: satelError } = useWidgetAPI(widget, "satellites");
 
   if (dashError) {
     return <Container service={service} error={dashError} />;
@@ -31,40 +31,44 @@ export default function Component({ service }) {
     );
   }
 
-  const diskUsage = dashData.diskSpace.available / dashData.diskSpace.used
-  const monthPayout = (payoutData.currentMonth.held + payoutData.currentMonth.payout) / 100
-  const onlineScore = 0
-  satelData.forEach(function (sat, index) {
-    onlineScore += sat.onlineScore
+  const diskUsage = dashData.diskSpace.used / dashData.diskSpace.available * 100;
+  let diskUsageStr = `${t("common.bytes", { value: dashData.diskSpace.used, decimals: 0 })}`;
+  diskUsageStr += ` (${t("common.percent", { value: diskUsage })})`;
+
+  const monthPayout = (payoutData.currentMonth.held + payoutData.currentMonth.payout) / 100;
+
+  let onlineScore = 0;
+  satelData.audits.forEach((sat) => {
+    onlineScore += sat.onlineScore;
   });
-  onlineScore /= satelData.length
+  // take the floor bc if availability is not 100%, we want to see it
+  onlineScore = Math.floor((onlineScore / satelData.audits.length) * 100 * 10) / 10;
 
   return (
     <Container service={service}>
       <Block
         label="storjnode.monthearn"
-        value={t("common.number", {
+        value={`$${t("common.number", {
           value: monthPayout,
           decimals: 2,
-        })}
+        })}`}
       />
       <Block
-        label="storjnode.diskusage"
-        value={t("common.percent", {
-          value: diskUsage,
-        })}
-      />
-      <Block
-        label="storjnode.bandwidthusage"
+        label="storjnode.monthbandwidthusage"
         value={t("common.bytes", {
           value: dashData.bandwidth.used,
           maximumFractionDigits: 1,
         })}
       />
       <Block
+        label="storjnode.diskusage"
+        value={ diskUsageStr }
+      />
+      <Block
         label="storjnode.onlinescore"
         value={t("common.percent", {
           value: onlineScore,
+          decimals: 1,
         })}
       />
     </Container>
